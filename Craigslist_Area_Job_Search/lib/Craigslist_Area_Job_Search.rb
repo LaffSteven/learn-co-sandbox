aaeca87824483871f8b6dfb6743e3442c3edcb1e
Steven Laff <steve.laff@gmail.com>
2019-10-08 22:37:09 +0000

    Done.

---
+++ Craigslist_Area_Job_Search/lib/Craigslist_Area_Job_Search.rb
@@ -0,0 +1,6 @@
+require "Craigslist_Area_Job_Search/version"
+
+module CraigslistAreaJobSearch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ Craigslist_Area_Job_Search/lib/version.rb
@@ -0,0 +1,3 @@
+module CraigslistAreaJobSearch
+  VERSION = "0.1.0"
+end
