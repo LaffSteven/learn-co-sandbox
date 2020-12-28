74fe523977f1d779d4314130db3d741200b029fa
Steven Laff <steve.laff@gmail.com>
2020-12-28 17:37:44 +0000

    Automatically backed up by Learn

--- job_search/lib/job_search.rb
+++
@@ -1,8 +0,0 @@
-require "job_search/version"
-
-module JobSearch
-  class Error < StandardError; end
-  # Your code goes here...
-end
-
-require_relative
--- job_search/lib/job_search/version.rb
+++
@@ -1,3 +0,0 @@
-module JobSearch
-  VERSION = "0.1.0"
-end
